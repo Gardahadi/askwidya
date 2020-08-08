4591d9697c64e0134f0321d95055e0b8d88071e1
Gardahadi <garda4199@gmail.com>
2020-08-08 20:49:49 +0700

    add linter and pre commit hook

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 import * as express from 'express';
+import 'dotenv/config';
 
 const app = express();
 
-
-//Set up dummy api route
+// Set up dummy api route
 app.get('/api', (req, res, next) => {
   res.status(200).json({
     hello: 'World!',
   });
 });
 
-const port: Number = Number(process.env.PORT) || 3000;
+const port: Number = number(process.env.PORT) || 3000;
 const startServer = async () => {
   await app.listen(port, () => {
-    console.log(`
-Server running on http://localhost:${port}
-`);
+//     console.log(`
+// Server running on http://localhost:${port}
+// `);
   });
 };
 
