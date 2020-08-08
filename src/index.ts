d402a208044e3e9539651b9919195e48d3bfaded
Gardahadi <garda4199@gmail.com>
2020-08-08 22:35:13 +0700

    setup typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 import * as express from 'express';
 import 'dotenv/config';
+import { createConnection } from 'typeorm';
 
-const app = express();
+// Create TypeORM Connection
+createConnection().then((_) => {
+  // create and setup express app
+  const app = express();
 
-// Set up dummy api route
-app.get('/api', (req, res, next) => {
-  res.status(200).json({
-    hello: 'World!',
+  // Set up dummy api route
+  app.get('/api', (_, res) => {
+    res.status(200).json({
+      hello: 'World!',
+    });
   });
+
+  const port: number = Number(process.env.PORT) || 3000;
+
+  const startServer = async () => {
+    await app.listen(port);
+  };
+
+  startServer();
 });
-
-const port: Number = number(process.env.PORT) || 3000;
-const startServer = async () => {
-  await app.listen(port, () => {
-//     console.log(`
-// Server running on http://localhost:${port}
-// `);
-  });
-};
-
-startServer();
