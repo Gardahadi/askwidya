e246670bda22310ca355511437e58b708b500011
Gardahadi <garda4199@gmail.com>
2020-08-09 21:59:08 +0700

    add conversation logic

--- src/controllers/BotController.ts
+++ src/controllers/BotController.ts
@@ -1,24 +1,70 @@
-// import { UserController } from './UserController';
+import { UserController } from "./UserController";
 
-// const messageController = new MessageController();
+const userController = new UserController();
 
 export class BotController {
   // Handles messages events
-  handleMessage(senderPsid, receivedMessage) {
-    const response = "Hi! My name is Widya, I dont think we've met before, whats your name?";
-
+  async handleMessage(psid, receivedMessage) {
     if (receivedMessage.text) {
       // check wether or not user is in database
-
-      this.sendMessage(senderPsid, response);
+      if (userController.isUserExist(psid)) {
+        if (userController.isFirstnameExist(psid)) {
+          if (userController.isBirthdayExist(psid)) {
+            this.sendMessage(psid, this.getBirthdayMessage());
+          } else {
+            await this.saveBirthday(psid, receivedMessage);
+            await this.askService(psid);
+          }
+        } else {
+          await this.saveFirstName(psid, receivedMessage);
+          await this.askBirthday(psid);
+        }
+      } else {
+        await this.saveUser(psid);
+        await this.askFirstName(psid);
+      }
     }
+  }
+
+  async getBirthdayMessage() {
+    return "still cant count yet";
+  }
+
+  async saveUser(newPsid) {
+    await userController.saveNewUser({
+      psid: newPsid,
+    });
+  }
+
+  async askFirstName(psid) {
+    const response = "Hi! My name is Widya. I don't think we've met before, may I know your first name?";
+    this.sendMessage(psid, response);
+  }
+
+  async saveFirstName(psid, name) {
+    await userController.saveFirstName(psid, name);
+  }
+
+  async askBirthday(psid) {
+    const response = "May I know your birthday?";
+    this.sendMessage(psid, response);
+  }
+
+  async saveBirthday(psid, birthday) {
+    await userController.saveBirthday(psid, birthday);
+  }
+
+  async askService(psid) {
+    const response = "Would you like to know when your birthday is";
+
+    this.sendMessage(psid, response);
   }
 
   // Sends response messages via the Send API
   async sendMessage(senderPsid, response) {
     const fetch = require("node-fetch");
     // Construct the message body
-    const request_body = {
+    const requestBody = {
       messaging_type: "RESPONSE",
 
       recipient: {
@@ -35,7 +81,7 @@ export class BotController {
     const res = await fetch(uri, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(request_body),
+      body: JSON.stringify(requestBody),
     });
     console.log(res);
   }
